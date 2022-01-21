7d2a40d1361e561f1be36c83804839c47689ec9b
Ankit Khandelwal <ankitkh642@gmail.com>
2022-01-21 16:34:21 +0530

    Used constructor functions

--- JavaScript/q2.js
+++ JavaScript/q2.js
@@ -1,35 +1,38 @@
-class MetadataParser{
-  constructor(version,channel,keyField){
+function MetadataParser(version,channel,keyField){
+  this.version=version;
+  this.channel=channel;
+  this.keyField=keyField;
+  
+  this.setVersion=function(version){
     this.version=version;
+  }
+  
+  this.setChannel=function(chanel){
     this.channel=channel;
+  }
+  
+  this.setkeyField=function(keyField){
     this.keyField=keyField;
   }
-  setVersion(version){
-    this.version=version;
-  }
-  setChannel(version){
-    this.version=version;
-  }
-  setkeyField(version){
-    this.version=version;
-  }
-  getVersion()
+  
+  this.getVersion=function()
   {
     return this.version;
   }
-  getChannel()
+  this.getChannel=function()
   {
     return this.channel;
   }
-  getkeyField()
+  this.getkeyField=function()
   {
     return this.keyField;
   }
-  getKeyFields(arr){
-    return arr.map((obj)=>{
+}
+
+MetadataParser.prototype.getKeyFields=function(arr){
+  return arr.map((obj)=>{
         return obj.keyField;
     })
-  }
 }
 const a=new MetadataParser(10,'A','ABC');
 const b=new MetadataParser(12,'B','DEF');
--- JavaScript/q4.js
+++ JavaScript/q4.js
@@ -1,33 +1,28 @@
-class SortArray{
-  constructor(...arr)
-  {
-    this.arr=arr;
-  }
+function SortArray(...arr){
+  this.arr=arr;
   
-  #sortArray(arr) {
+  sortArray=function(arr){
   return arr.sort((a,b)=>{
             return a - b
       })
   }
   
-  getSortedArray(){
-    return this.#sortArray(this.arr);
+  this.getSortedArray=function(){
+    return sortArray(this.arr);
   }
 }
-class SortObjectArray extends SortArray{
-  constructor(key,...arr){
-    super(...arr)
-    this.key=key;
-  }
+function SortObjectArray(key,...arr){
+  this.key=key;
+  SortArray.call(this,...arr)
   
-  #sortArray(arr,key) {
+  sortArray=function(arr,key){
   return arr.sort((a,b)=>{
             return a[key] - b[key]
       })
   }
   
-  getSortedArray(){
-    return this.#sortArray(this.arr,this.key);
+  this.getSortedArray=function(){
+    return sortArray(this.arr,this.key);
   }
 }
 const a=new SortArray(1,2,4,3,4,5,6);
